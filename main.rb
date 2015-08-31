73d05c25d3c97aaef0af13bfffb0c6e43be740ab
Christopher Leabeter <christopherleabeter@gmail.com>
2015-08-31 18:06:22 -0400

    Layers of subclasses built and mapped. Testing inheritance with blocks of commented code before creating final version.

---
+++ main.rb
@@ -0,0 +1,60 @@
+# require_relative "animal.rb"
+require_relative "frog.rb"
+require_relative "bat.rb"
+require_relative "parrot.rb"
+require_relative "chimpanzee.rb"
+
+
+# animal_example = Animal.new("animalio")
+# puts animal_example.name
+# puts animal_example.num_of_legs
+# puts animal_example.method_of_movement
+# animal_example.warm_blooded?
+
+# amphibian_example = Amphibian.new("amphibio")
+# puts amphibian_example.name
+# puts amphibian_example.num_of_legs
+# puts amphibian_example.method_of_movement
+# puts amphibian_example.warm_blooded?
+
+# mammal_example = Mammal.new("mammalio")
+# puts mammal_example.name
+# puts mammal_example.num_of_legs
+# puts mammal_example.method_of_movement
+# puts mammal_example.warm_blooded?
+
+# primate_example = Primate.new("primatthew")
+# puts primate_example.name
+# puts primate_example.num_of_legs
+# puts primate_example.method_of_movement
+# puts primate_example.warm_blooded?
+
+# bird_example = Bird.new("birdbrain")
+# puts bird_example.name
+# puts bird_example.num_of_legs
+# puts bird_example.method_of_movement
+# puts bird_example.warm_blooded?
+
+bat_example = Bat.new("batman")
+puts bat_example.name
+puts bat_example.num_of_legs
+puts bat_example.method_of_movement
+puts bat_example.warm_blooded?
+
+frog_example = Frog.new("frogger")
+puts frog_example.name
+puts frog_example.num_of_legs
+puts frog_example.method_of_movement
+puts frog_example.warm_blooded?
+
+chimpanzee_example = Chimpanzee.new("Johnny Chimpo")
+puts chimpanzee_example.name
+puts chimpanzee_example.num_of_legs
+puts chimpanzee_example.method_of_movement
+puts chimpanzee_example.warm_blooded?
+
+parrot_example = Parrot.new("Polly")
+puts parrot_example.name
+puts parrot_example.num_of_legs
+puts parrot_example.method_of_movement
+puts parrot_example.warm_blooded?
